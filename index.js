a930cadbadbe89c51cae8e9792a28f7d5d132b6e
Jose Kupeka Muhlanga <jose.muhlanga@student.moringaschool.com>
2022-09-01 23:59:03 +0300

    Linking JSOn file to Web Page and Displaying Relevant Data

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 document.addEventListener("DOMContentLoaded",()=>{
     console.log("The DOM has Loaded");
+    fetchEvents();
 })
 
+function fetchEvents(){
+    fetch(`http://localhost:3000/events`)
+    .then((response)=> response.json())
+    .then((data) => renderEvents(data))
+}
 
-
+function renderEvents (data){
+    const eventSection = document.querySelector(`#eventList`);
+    data.forEach(someEvent => {
+        const li =document.createElement(`li`);
+        li.innerHTML = `${someEvent.eventName},on ${someEvent.date} at ${someEvent.venue} game: ${someEvent.game} hosted by :${someEvent.host}`;
+        eventSection.appendChild(li);
+    });
+}
